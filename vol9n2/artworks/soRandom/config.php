5b025443430245d8c15125f4c2ebcfaf5f946567
Mark NeuCollins <mneucollinsw@gmail.com>
2015-07-13 16:30:55 -0500

    additional removal of sensitive info

--- vol9n2/artworks/soRandom/config.php
+++ vol9n2/artworks/soRandom/config.php
@@ -3,9 +3,5 @@
 	//fill in with appropriate info for your server
 	//$freshInstall=1; //delete this line if you're attempting a manual config
 	require_once("../../../db_connect.php");
-//$dbHost = "localhost"; //database host address
-//$dbName = "tirw"; // database name
-//$dbUser = "iowareview_u"; // database user name
-//$dbPassword = "cKq9cL2TeYyMaRyy"; // database password	
 ?>
 	
